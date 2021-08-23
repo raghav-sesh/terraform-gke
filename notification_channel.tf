b0fc64b788116a4b53100b01b2f8e60478ff9286
raghav-sesh <raghav.seshadri.r@gmail.com>
2021-08-22 19:31:24 -0700

    Alerts and notification channel
    Added terraform files to create basic alerts for cpu & memory usage and notify through email

---
+++ notification_channel.tf
@@ -0,0 +1,7 @@
+resource "google_monitoring_notification_channel" "email_devops" {
+  display_name = "DevOps Notification Channel"
+  type         = "email"
+  labels = {
+    email_address = "rseshadr@usc.edu"
+  }
+}
